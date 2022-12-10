1726c3e85f02e31ccf93087dbe2592125a407ca0
Oguzhan Soyalp <soyalpoguz@gmail.com>
2022-12-11 00:09:51 +0300

    add change main branch test

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
-
+//değişim
         // can we add smt ınn  here
         // solved ıdk
         //i added somethıng ın to the branch or branch
